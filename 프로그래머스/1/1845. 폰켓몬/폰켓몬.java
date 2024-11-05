f28430d052f0d0a3b7945b220af3d3db556035c3
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2024-11-05 14:17:13 +0900

    [level 1] Title: 폰켓몬, Time: 4.29 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,19 +2,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         
-        // 최대 폰켓몬 수
-        int max=nums.length/2;
-        
         // 폰켓몬 set(중복 방지)
-        Set<Integer> pocket=new HashSet<>();
+        HashSet<Integer> pocket=new HashSet<>();
         
         // set에 폰켓몬을 넣는다
-        for(int n:nums){
-            pocket.add(n);
+        for(int i=0;i<nums.length;i++){
+            pocket.add(nums[i]);
         }
         
-        // 폰켓몬 최대 수와 set 사이즈 중 큰 값 반환
-        return max<pocket.size()? max:pocket.size();
+        if(pocket.size()>nums.length/2)
+            return nums.length/2;
         
+        return pocket.size();
     }
 }
