3e3e129cbc620307dccb2c05016e7d94379bd4de
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2024-11-21 12:52:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        int[] check={0,0,0,0,0,0,0,0,0,0};
+        
+        for(int num:numbers){
+            check[num]=1;
+        }
+        
+        for(int i=0;i<10;i++){
+            if(check[i]==0)
+                answer=answer+i;
+        }
+        
+        return answer;
+    }
+}
