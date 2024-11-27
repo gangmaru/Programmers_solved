a5147a14bae5071e5f9cb2193c93d6fcc6df97b1
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2024-11-27 10:23:09 +0900

    [level 1] Title: 직사각형 별찍기, Time: 289.78 ms, Memory: 95.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+
+        for(int i=0;i<column;i++){
+            for(int j=0;j<row;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
