9ac356b803473f5394c6ed677b5d151a2557afc9
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2024-11-27 10:32:01 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.07 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int gcd=0;
+        
+        for(int i=1;i<=n && i<=m;i++){
+            if(n%i==0 && m%i==0){
+                gcd=i;
+            }
+        }
+        answer[0]=gcd;
+        answer[1]=(n*m)/gcd;
+        
+        return answer;
+    }
+}
