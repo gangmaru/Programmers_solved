47dc542cc8490fd36daff578ff1b1674691ed39c
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2025-10-21 22:56:01 +0900

    [Silver II] Title: 스택 수열, Time: 2584 ms, Memory: 38876 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,31 @@
+n=int(input())
+a=[0]*n
+
+for i in range(n):
+    a[i]=int(input())
+
+stack=[]
+num=1
+result=True
+answer=[]
+
+for i in range(n):
+    su=a[i]
+    if su>=num:
+        while su>=num:
+            stack.append(num)
+            num+=1
+            answer.append('+')
+        stack.pop()
+        answer.append('-')
+    else:
+        n=stack.pop()
+        if n>su:
+            print('NO')
+            result=False
+            break
+        else:
+            answer.append('-')
+if result:
+    for i in answer:
+        print(i)
