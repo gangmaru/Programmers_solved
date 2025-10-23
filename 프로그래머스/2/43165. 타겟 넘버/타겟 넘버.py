c26f0290639e972418dacb2c0d4ce5e3b824b32a
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2025-10-23 23:02:10 +0900

    [level 2] Title: 타겟 넘버, Time: 381.14 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,20 @@
+# def solution(numbers, target):
+#     def dfs(cnt, current_sum):
+#         if cnt == len(numbers):
+#             if current_sum == target:
+#                 return 1
+#             else:
+#                 return 0
+#         return dfs(cnt + 1, current_sum + numbers[cnt]) + dfs(cnt + 1, current_sum - numbers[cnt])
+#     return dfs(0, 0)
+
+
 def solution(numbers, target):
-    def dfs(cnt, current_sum):
-        if cnt == len(numbers):
-            if current_sum == target:
+    def dfs(idx, total):
+        if idx==len(numbers):
+            if total==target:
                 return 1
             else:
                 return 0
-
-        return dfs(cnt + 1, current_sum + numbers[cnt]) + dfs(cnt + 1, current_sum - numbers[cnt])
-
-    return dfs(0, 0)
+        return dfs(idx+1, total+numbers[idx])+dfs(idx+1, total-numbers[idx])
+    return dfs(0,0)
