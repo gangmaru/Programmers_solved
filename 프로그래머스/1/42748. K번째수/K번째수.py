9bd3c0c4589a06a152e292a3e4403f7ab06b42b5
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2025-11-06 15:39:05 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,17 @@
+# def solution(array, commands):
+#     answer = []
+    
+#     for i,j,k in commands:
+#         slice=array[i-1:j]
+#         slice=sorted(slice)
+#         answer.append(slice[k-1])
+#     return answer
+
 def solution(array, commands):
     answer=[]
-    
     for i,j,k in commands:
-        slice=array[i-1:j]
-        slice=sorted(slice)
-        answer.append(slice[k-1])
-    
+        arr=array[i-1:j]
+        arr=sorted(arr)
+        arr=arr[k-1]
+        answer.append(arr)
     return answer
