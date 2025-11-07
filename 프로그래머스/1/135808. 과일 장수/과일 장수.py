821447c0dd8ea02710fa645c3b8eb2cc5bbf4b1c
HyunJung Shim <107922545+gangmaru@users.noreply.github.com>
2025-11-07 11:58:44 +0900

    [level 1] Title: 과일 장수, Time: 100.15 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,8 +1,20 @@
 def solution(k, m, score):
     answer = 0
+    box=len(score)//m
+    if len(score)<m:
+        return 0
     score.sort(reverse=True)
+    # for box_start in range(0, len(score) - m + 1, m):
+    #     box_min = score[box_start + m - 1]  # 해당 상자에서 가장 낮은 점수
+    #     answer += box_min * m
     
-    for i in range(0, len(score), m):
-        if len(score[i:i+m]) == m:
-            answer += min(score[i:i+m]) * m
+    for i in range(box):
+        box_min=score[(i+1)*m-1]
+        answer+=box_min*m
+    # print(score)
+    # for i in range(box):
+    #     s=score[-m*(i+1):]
+    #     # print(s)
+    #     answer+=min(s)*m
+    
     return answer
